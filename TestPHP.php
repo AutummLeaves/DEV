0562e913c4202ecaa509d15b985dc6b3f448ad95
AutummLeaves <47726988+AutummLeaves@users.noreply.github.com>
2019-02-20 18:07:41 +0100

    ..

--- TestPHP.php
+++ TestPHP.php
@@ -1,55 +1,79 @@
 <!DOCTYPE html>
-<html>
-<body>
+<html lang="en-US"> <!--Taal waarin de pagina wordt weergeven-->
+<meta charset="UTF-8" > <!--Encoding die je pagina gebruikt-->
+<meta name=description content="PHP Test Page">
+<meta name="keywords" content="PHP">
 
-<h1>My first PHP page</h1>
+<link rel="icon" href="\docs\Sources\Infinite_tsukuyomi.png">
+<link rel="stylesheet" type="text/css" href="style.css">
 
-<h2>HTML Form</h2>
+<body class="page">
+    <div class="container">
 
-<form class="form" action="mailto:devettegangster@hotmail" method="post" enctype="text/plain">
-        <div class="formgroup">
-            <label>First Name</label>
-            <input type="text" name="firstName" placeholder="Enter your name">
-        </div>
-    <br>
-        <div class="formgroup">
-            <label>Last Name</label>
-            <input type="text" name="lastName" placeholder="Enter your last name">
-        </div>
-    <br>
-        <div class="formgroup">
-            <label>Email</label>
-            <input type="text" name="email" placeholder="Enter your Email">
-        </div>
-    <br>
-        <div class="formgroup">
-            <label>Birthday:</label>
-            <input type="date" name="birthday">
-        </div>
-    <br>
-        <div class="formgroup">
-            <label>Gender</label>
-            <select name="gender">
-                <option value="male">Male</option>
-                <option value="Female">Female</option>
-            </select>
-        </div>
-    <br>
-        <div class="radio">
-            <input type="radio" name="Idk" value="Iets1" checked> Iets1<br>
-            <input type="radio" name="Idk" value="Iets2"> Iets2<br>
-            <input type="radio" name="Idk" value="Iets3"> Iets3
-        </div>
-    <br>
-        <div class="formgroup">
-            <textarea class="formgroup" name="message" rows="10" cols="128" placeholder="Enter your comment here"></textarea>
-        </div>
-    <br>
-    <input type="submit" name="submit" value="Submit" action="popup" 
-  onclick="window.open('sabon.html','popup','width=600,height=400')">
-  
-</form>
-    
+        <h1>My first PHP page</h1>
+
+        <h2>HTML Form</h2>
+
+        <form class="form" action="" method="post" enctype="text/plain">
+            <div class="formgroup">
+                <label>First Name</label>
+                <input type="text" name="firstName" placeholder="Enter your name">
+            </div>
+
+            <br>
+
+            <div class="formgroup">
+                <label>Last Name</label>
+                <input type="text" name="lastName" placeholder="Enter your last name">
+            </div>
+
+            <br>
+
+            <div class="formgroup">
+                <label>Email</label>
+                <input type="text" name="email" placeholder="Enter your Email">
+            </div>
+
+            <br>
+
+            <div class="formgroup">
+                <label>Birthday:</label>
+                <input type="date" name="birthday">
+            </div>
+
+            <br>
+
+            <div class="formgroup">
+                <label>Gender</label>
+                <select name="gender">
+                    <option value="male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+            </div>
+
+            <br>
+
+            <div class="radio">
+                <input type="radio" name="Idk" value="Iets1" checked> Iets1<br>
+                <input type="radio" name="Idk" value="Iets2"> Iets2<br>
+                <input type="radio" name="Idk" value="Iets3"> Iets3
+            </div>
+
+            <br>
+
+            <div class="formgroup">
+                <textarea class="formgroup" name="message" rows="10" cols="95" placeholder="Enter your comment here"></textarea>
+            </div>
+
+            <br>
+
+            <input type="submit" name="submit" value="Submit">
+            
+        </form>
+    </div>
+</body>
+
+
 <?php
 echo "My first PHP script! <br>"; // print
 
@@ -88,7 +112,9 @@ function myTest() {
     echo "<p>Variable x inside function is: $x</p>";
     $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
 } 
+
 myTest();
+
 echo $y;
 
 echo "<p>Variable x outside function is: $x</p>";
@@ -100,5 +126,4 @@ echo "$firstname $lastname";
 
 ?> 
 
-</body>
 </html>
