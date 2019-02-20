11cb06f66cd92017563311e6201c4feceb89f1d9
AutummLeaves <47726988+AutummLeaves@users.noreply.github.com>
2019-02-20 15:59:47 +0100

    Update TestPHP.php

--- TestPHP.php
+++ TestPHP.php
@@ -6,15 +6,48 @@
 
 <h2>HTML Form</h2>
 
-<form action="TestPHP.php" method="post">
-  First name:<br>
-  <input type="text" name="firstname">
-  <br>
-  Last name:<br>
-  <input type="text" name="lastname">
-  <br><br>
-  <input type="submit" value="Submit">
-
+<form class="form" action="mailto:devettegangster@hotmail" method="post" enctype="text/plain">
+        <div class="formgroup">
+            <label>First Name</label>
+            <input type="text" name="firstName" placeholder="Enter your name">
+        </div>
+    <br>
+        <div class="formgroup">
+            <label>Last Name</label>
+            <input type="text" name="lastName" placeholder="Enter your last name">
+        </div>
+    <br>
+        <div class="formgroup">
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Enter your Email">
+        </div>
+    <br>
+        <div class="formgroup">
+            <label>Birthday:</label>
+            <input type="date" name="birthday">
+        </div>
+    <br>
+        <div class="formgroup">
+            <label>Gender</label>
+            <select name="gender">
+                <option value="male">Male</option>
+                <option value="Female">Female</option>
+            </select>
+        </div>
+    <br>
+        <div class="radio">
+            <input type="radio" name="Idk" value="Iets1" checked> Iets1<br>
+            <input type="radio" name="Idk" value="Iets2"> Iets2<br>
+            <input type="radio" name="Idk" value="Iets3"> Iets3
+        </div>
+    <br>
+        <div class="formgroup">
+            <textarea class="formgroup" name="message" rows="10" cols="128" placeholder="Enter your comment here"></textarea>
+        </div>
+    <br>
+    <input type="submit" name="submit" value="Submit" action="popup" 
+  onclick="window.open('sabon.html','popup','width=600,height=400')">
+  
 </form>
     
 <?php
